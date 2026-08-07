c166679fa26cd81121446a474bc003672c6bcaaa
MinhDat1312 <nguyenthangdat84@gmail.com>
2026-08-07 10:55:29 +0700

    feat: environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,16 @@
 export const environment = {
     production: false,
 
-    authApiUrl: "",
-    adminApiUrl: "",
-    clientApiUrl: "",
+    authApiUrl: process.env['NG_APP_AUTH_API_URL'] || "http://localhost:4400",
+    adminApiUrl: process.env['NG_APP_ADMIN_API_URL'] || "http://localhost:4300",
+    clientApiUrl: process.env['NG_APP_CLIENT_API_URL'] || "http://localhost:4200",
 
-    apiUrl: "",
-    backendCallbackUrl: "",
+    apiUrl: process.env['NG_APP_API_URL'] || "http://localhost:7070",
+    backendCallbackUrl: process.env['NG_APP_BACKEND_CALLBACK_URL'] || "http://localhost:7071/callback",
 
     keycloak: {
-        url: "",
-        realm: "",
-        clientId: ""
+        url: process.env['NG_APP_KEYCLOAK_URL'] || "http://localhost:8082",
+        realm: process.env['NG_APP_KEYCLOAK_REALM'] || "goatsports",
+        clientId: process.env['NG_APP_KEYCLOAK_CLIENT_ID'] || "goatsports-main"
     }
 };
