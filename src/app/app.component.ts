4f5470f4fded2853e129dddc237980b16465923d
MinhDat1312 <nguyenthangdat84@gmail.com>
2026-09-23 17:06:38 +0700

    feat: shared Three.js boot splash for client, admin and auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { hideBootSplashWhenReady } from './boot-splash';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Goat Sports';
+
+  constructor(router: Router) {
+    hideBootSplashWhenReady(router);
+  }
 }
